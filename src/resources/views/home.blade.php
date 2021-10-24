b55a1096991dc8b08649f9696d35e3851ba0028f
Alyxia Sother <lexisoth2005@gmail.com>
2021-10-24 17:29:59 +0200

    [Home] Add profile picture

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -6,10 +6,23 @@
 
 @section('content')
 <div class="mt-8 overflow-hidden bg-white rounded-lg shadow font dark:bg-gray-800">
-    <div class="grid grid-cols-1 md:grid-cols-2">
+    <div class="flex justify-center pt-8 m-5 sm:pt-0">
+        {{-- TODO: Fetch Discord avatar --}}
+        <img class="text-gray-700 rounded-full w-60" src="./images/small_blue.png" />
+    </div>
+    <div class="flex-col m-5 text-center">
+        <div class="text-lg font-semibold text-gray-400">
+            Alyxia Sother
+        </div>
+        <div class="text-sm text-gray-500">
+            Welcome to my little site!
+        </div>
+    </div>
+    {{-- TODO: Find content to put here --}}
+    {{-- <div class="grid grid-cols-1 md:grid-cols-2">
         @carditem(['title' => 'Alyxia Sother'])
         Welcome to my little site!
         @endcarditem
-    </div>
+    </div> --}}
 </div>
 @endsection
--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -15,4 +15,5 @@ mix.js('resources/js/app.js', 'public/js')
     // .sass('resources/sass/app.scss', 'public/css')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
-    ]);
+    ])
+    .copyDirectory('resources/images', 'public/images');
